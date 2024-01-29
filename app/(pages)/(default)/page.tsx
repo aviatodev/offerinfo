f26a07b9f05011e88693297acf3f99b4fe5721ad
aviatodev <aviatodevelopment@gmail.com>
2024-01-30 02:31:54 +0800

    bugfix<deployment>: Largest Contentful Paint

--- app/(pages)/(default)/page.tsx
+++ app/(pages)/(default)/page.tsx
@@ -3,20 +3,22 @@ import CompanyLogoDisplay from "../../components/CompanyLogoDisplay";
 
 export default function Welcome() {
 	return (
-		<div className="text-4xl text-center justify-center items-center mt-1 mb-1 lg:mt-4 lg:mb-8">
-			<p className="text-sm lg:text-lg xl:text-2xl mb-4">
-				A database to assist your job hunting
-			</p>
-			<p className="text-xs lg:text-base">
-				Contributed by our kind applicants
-			</p>
+		<main className="mt-16 md:mt-20 max-w-7xl m-auto">
+			<div className="text-4xl text-center justify-center items-center mt-1 mb-1 lg:mt-4 lg:mb-8">
+				<p className="text-sm lg:text-lg xl:text-2xl mb-4">
+					A database to assist your job hunting
+				</p>
+				<p className="text-xs lg:text-base">
+					Contributed by our kind applicants
+				</p>
 
-			<CompanyLogoDisplay />
+				<CompanyLogoDisplay />
 
-			<p className="text-sm lg:text-lg xl:text-2xl mb-4">
-				Leave your interview progress and feedback here
-			</p>
-			<p className="text-xs lg:text-base">Thank you!</p>
-		</div>
+				<p className="text-sm lg:text-lg xl:text-2xl mb-4">
+					Leave your interview progress and feedback here
+				</p>
+				<p className="text-xs lg:text-base">Thank you!</p>
+			</div>
+		</main>
 	);
 }
